da1e209d02c1b93a14e923ee483df5b7519e5858
Flako <obiasong.arrey@go-groups.net>
2021-08-27 10:12:49 +0100

    Initial push from local

---
+++ checkguess.py
@@ -0,0 +1,6 @@
+def compareGuess(x, y):
+    correct = False
+    if x==y:
+        correct = True
+
+    return correct
---
+++ guess.py
@@ -0,0 +1,27 @@
+import random
+import checkguess
+name =  input("Enter your name and press enter to start the game: ")
+
+number  =  random.randint(1, 10)
+
+attempts_limit = 3
+attempts = 0
+
+
+while attempts < attempts_limit:
+    try:
+        guess = int(input("Enter a number between 1 and 10 (including the endpoints): "))
+        if guess < 1 or guess > 10:
+            raise ValueError("Please enter a number between 1 and 10 from inside")
+        if checkguess.compareGuess(number, guess):
+            print(f"Great Job {name}!! You guessed correctly the number is {number}")
+            break
+        else :
+            print(f"You almost got it {name}, Try again!")
+            pass
+    except ValueError:
+        print("Please enter a valid number between 1 and 10") 
+    attempts += 1    
+else :
+    print(f"Sorry {name}! You have not been able to guess the number after {attempts_limit} attempts.")
+    exit(0)
